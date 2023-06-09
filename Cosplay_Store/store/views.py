6a6c64d09ab5eea44de4c5591bdd569eadc7776c
MrWildcode <121079532+MrWildcode@users.noreply.github.com>
2023-06-09 14:22:05 -0700

    Filters created

--- Cosplay_Store/store/views.py
+++ Cosplay_Store/store/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from store.models import Products
+from store.serializers import ProductsSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductsViewSet(ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['universe']
+    search_fields = ['name', 'universe']
+    ordering_fields = ['price', 'universe']
+
+
+
