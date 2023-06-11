39b096ec73487e52f81eb9dc2ff4a51e04992422
MrWildcode <121079532+MrWildcode@users.noreply.github.com>
2023-06-11 13:10:39 -0700

    UserProductRelation model created

--- Cosplay_Store/store/models.py
+++ Cosplay_Store/store/models.py
@@ -11,7 +11,28 @@ class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     universe = models.CharField(choices=UNIVERSE_CHOICES)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                              related_name='my_products')
+    watchers = models.ManyToManyField(User, through='UserProductRelation',
+                                      related_name='products')
 
     def __str__(self):
         return f'ID {self.id}: {self.name}'
+
+class UserProductRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Terrible'),
+        (2, 'Bad'),
+        (3, 'Ok'),
+        (4, 'Good'),
+        (5, 'Perfect'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+    like = models.BooleanField(default=False)
+    in_cart = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.product.name}, RATE {self.rate}'
